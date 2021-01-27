7d7137d3dc64a80701e5cbf82a98efb10f2a9057
Yves Parès (Ywen) <yves.pares@gmail.com>
2021-01-27 15:16:33 +0100

    Added instance of GenericRec for ARec

--- vinyl-example/app/Main.hs
+++ vinyl-example/app/Main.hs
@@ -1,31 +1,20 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings, OverloadedLabels, TypeOperators, DataKinds, TypeFamilies, FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, OverloadedLabels, TypeOperators,
+  DataKinds, TypeFamilies, FlexibleContexts, FlexibleInstances,
+  UndecidableInstances, StandaloneDeriving, MultiParamTypeClasses #-}
 module Main where
 
 import Database.Selda hiding ((:*:), from)
 import qualified Database.Selda as S
+import Database.Selda.Debug (compile)
 import Database.Selda.SQLite
 import Data.Vinyl
+import Data.Vinyl.ARec
+import Data.Vinyl.TypeLevel
 import Data.Kind (Type)
-import Data.Typeable (Typeable)
+import Data.Typeable (Typeable, Proxy(..))
 import GHC.TypeLits
 import GHC.Generics
 
-
--- | We make this newtype in order to make a Generic instance for Rec that looks
--- exactly like the plain instance that would have been derived from the
--- equivalent classical Haskell record. It doesn't support empty records.
-newtype GenRec rs = GenRec { unGenRec :: Rec ElField rs }
-
-class GenericRec ( rs :: [(Symbol,*)] ) where
-  type RecRep rs :: Type -> Type
-
-  fromRec :: Rec ElField rs -> RecRep rs x
-  toRec :: RecRep rs x -> Rec ElField rs
-
-instance (TypeError ('Text "Empty records are not supported by GenRec")) => GenericRec '[] where
-  type RecRep '[] = RecRep '[ "error" ::: () ]
-  fromRec = error "unreachable"
-  toRec = error "unreachable"
 
 type FieldRep s a =
   S1 ('MetaSel ('Just s)
@@ -34,25 +23,60 @@ type FieldRep s a =
                'DecidedStrict)
      (Rec0 a)
 
-instance (GenericRec (fld2 ': rs), KnownSymbol s)
-  => GenericRec ((s ::: a) ': (fld2 ': rs)) where
-  type RecRep ((s ::: a) ': (fld2 ': rs)) =
+type family RecRep rs :: Type -> Type where
+  RecRep '[] = TypeError ('Text "Empty records are not supported by GenericRec")
+  RecRep ((s ::: a) ': (fld2 ': rs)) =
     FieldRep s a :*: RecRep (fld2 ': rs)
+  RecRep ((s ::: a) ': '[]) = FieldRep s a
+
+class GenericRec r ( rs :: [(Symbol,*)] ) where
+  fromRec :: r ElField rs -> RecRep rs x
+  toRec :: RecRep rs x -> r ElField rs
+
+instance (TypeError ('Text "Empty records are not supported by GenericRec"))
+  => GenericRec r '[] where
+  fromRec = error "unreachable"
+  toRec = error "unreachable"
+
+instance (GenericRec Rec (fld2 ': rs), KnownSymbol s)
+  => GenericRec Rec ((s ::: a) ': (fld2 ': rs)) where
   fromRec (Field x :& xs@(_ :& _)) = M1 (K1 x) :*: fromRec xs
+  {-# INLINE fromRec #-}
   toRec (M1 (K1 x) :*: xs) = Field x :& toRec xs
+  {-# INLINE toRec #-}
 
-instance (KnownSymbol s) => GenericRec ((s ::: a) ': '[]) where
-  type RecRep ((s ::: a) ': '[]) = FieldRep s a
+instance (KnownSymbol s) => GenericRec Rec ((s ::: a) ': '[]) where
   fromRec (Field x :& RNil) = M1 (K1 x)
+  {-# INLINE fromRec #-}
   toRec (M1 (K1 x)) = Field x :& RNil
+  {-# INLINE toRec #-}
 
-instance (GenericRec rs) => Generic (GenRec rs) where
-  type Rep (GenRec rs) =
+instance ( GenericRec Rec rs, RecApplicative rs, RPureConstrained (IndexableField rs) rs
+         , NatToInt (RLength rs) )
+  => GenericRec ARec rs where
+  fromRec = fromRec . fromARec
+    -- Normally this should get fully inlined and fromARec should be optimized
+    -- away. If perf problems happen we should check
+  {-# INLINE fromRec #-}
+  toRec = toARec . toRec
+  {-# INLINE toRec #-}
+
+-- | We make this newtype in order to make a Generic instance for Rec that looks
+-- exactly like the plain instance that would have been derived from the
+-- equivalent classical Haskell record. It doesn't support empty records.
+newtype GenRec r (rs :: [(Symbol,*)]) = GenRec { unGenRec :: r ElField rs }
+-- deriving instance (RMap rs, RecordToList rs, ReifyConstraint Show ElField rs) => Show (GenRec r rs)
+
+instance (GenericRec r rs) => Generic (GenRec r rs) where
+  type Rep (GenRec r rs) =
     C1 ('MetaCons "GenRec" 'PrefixI 'True) (RecRep rs)
   from (GenRec r) = M1 (fromRec r)
+  {-# INLINE from #-}
   to (M1 x) = GenRec $ toRec x
+  {-# INLINE to #-}
 
-instance (GenericRec rs, Typeable rs, GSqlRow (RecRep rs)) => SqlRow (GenRec rs)
+instance (GenericRec r rs, Typeable r, Typeable rs, GSqlRow (RecRep rs))
+  => SqlRow (GenRec r rs)
 
 
 
@@ -60,21 +84,23 @@ data Pet = Dog | Horse | Dragon
   deriving (Show, Read, Bounded, Enum)
 instance SqlType Pet
 
-type Person = GenRec '["name" ::: Text, "age" ::: Int, "pet" ::: Maybe Pet] 
+type Person = GenRec ARec '["name" ::: Text, "age" ::: Int, "pet" ::: Maybe Pet] 
 
 people :: Table Person
 people = table "people" [#name :- primary]
 
 main = withSQLite "people.sqlite" $ do
   tryCreateTable people
-  insert_ people $ map GenRec $
+  insert_ people $ map (GenRec . toARec) $
     [ #name =: "Velvet"    :& #age =: 19 :& #pet =: Just Dog    :& RNil
     , #name =: "Kobayashi" :& #age =: 23 :& #pet =: Just Dragon :& RNil
     , #name =: "Miyu"      :& #age =: 10 :& #pet =: Nothing     :& RNil
     ]
 
-  adultsAndTheirPets <- query $ do
-    person <- select people
-    restrict (person ! #age .>= 18)
-    return (person ! #name S.:*: person ! #pet)
+  let theQuery = do
+        person <- select people
+        restrict (person ! #age .>= 18)
+        return (person ! #name S.:*: person ! #pet)
+  liftIO $ print $ compile theQuery
+  adultsAndTheirPets <- query theQuery
   liftIO $ print adultsAndTheirPets
